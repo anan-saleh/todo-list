ec9aeb799ee1e524bd0db90c2326b49194e652aa
anan-saleh <dingwuying@tutanota.com>
2023-06-15 18:04:53 +0300

    Add undelete option to the deleted todos

--- src/components/DeletedTodo/index.tsx
+++ src/components/DeletedTodo/index.tsx
@@ -1,16 +1,26 @@
 import { FC } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { reduxState } from '../../assets/interface';
+import { deleteAction } from '../../actions/todo-actions';
+
+import './style.css';
 
 export const DeletedTodo: FC = () => {
   const todoListState = useSelector((state: reduxState) => state.todoList);
+  const dispatch = useDispatch();
+
+  function unDeleteTodo(id: string) {
+    dispatch(deleteAction(id));
+  }
+
   return (
     <ul>
       {todoListState.length ? todoListState.map(({id, text, deleted}) => {
         if(deleted) {
           return (
-            <div key={id} className='todo-item-checked-container'>
+            <div key={id} className='todo-item-deleted-container'>
               <li>{text}</li>
+              <button onClick={() => unDeleteTodo(id)}>Undelete</button>
             </div>
           )
         }

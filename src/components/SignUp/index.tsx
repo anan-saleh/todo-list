87eac13c038435aee7366262aa4ee41ee02409b3
anan-saleh <dingwuying@tutanota.com>
2023-06-17 17:32:55 +0300

    Test connection to server

--- src/components/SignUp/index.tsx
+++ src/components/SignUp/index.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 export const SignUp = () => {
   function onSubmit (e: React.ChangeEvent<HTMLFormElement>) {
     e.preventDefault();
+    fetch(`${import.meta.env.VITE_API_SERVER_URL}/${import.meta.env.VITE_API_END_POINT}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: 'John',
+        email: 'john@example.com',
+        age: 25,
+      }),
+    })
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.error(error));
   }
   return (
     <>
       <form onSubmit={onSubmit}>
-        <button><Link to={'/dashboard'}>Sign up</Link></button>
+        <button>Sign up</button>
       </form>
     </>
   );
